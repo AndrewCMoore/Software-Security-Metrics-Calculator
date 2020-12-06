05a101bb1612314f25c13fb18574d1c01742ef59
AndrewCMoore <andrewmoore9815@gmail.com>
2020-12-06 10:07:31 -0500

    Reimplemented SampleHandler.java

--- src/ssmc/handlers/SampleHandler.java
+++ src/ssmc/handlers/SampleHandler.java
@@ -65,63 +65,22 @@ public class SampleHandler extends AbstractHandler {
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		//MessageDialog.openInformation(window.getShell(),	"SSMC",	"Hello, Eclipse world");
-		
-		//MessageDialog.openInformation(window.getShell(),"Root", getRoot().getLocation().toString());
-		//MessageDialog.openInformation(window.getShell(),"Path", getProjectName().toOSString());
-		//MessageDialog.openInformation(window.getShell(), "Methods", getMethods());
-
 		try {
-			for(int i =  0; i < getPackages().length; i++) {
-				if(getPackages()[i].getKind() == IPackageFragmentRoot.K_SOURCE){
-					//MessageDialog.openInformation(window.getShell(),"Package: " + i, getPackages()[i].getElementName());
-				}
-			}
+			addToTree();
 		} catch (CoreException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-
-		//AST 
-		try {
-			
-			for(int i = 0; i < getClasses().length; i++) {
-				generateMethodAST(getClasses()[i]);
-			}
-		
-			generateCommentAST(getClasses()[0]);
-			generateMethodAST(getClasses()[0]);
-			generateAttributeAST(getClasses()[0]);
-			generateStatementAST(getClasses()[0]);
-		} catch (CoreException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
+		}		
 		return null;
 	}
-	/**
-	 * Get the root directory of the Eclipse IDE folder. 
-	 * @return The IWorkspaceRoot representation of the root folder. 
-	 */
-	private IWorkspaceRoot getRoot() {
-		IWorkspaceRoot fileRoot = ResourcesPlugin.getWorkspace().getRoot();
-		//TreeNode<IWorkspaceRoot> root = new TreeNode<IWorkspaceRoot>(fileRoot);
-		return fileRoot;
-	}
 	
-	/**
-	 * Gets the current active project. Uses IStrucuted Selection and IWorkbenchWindow to identify the 
-	 * current active Eclipse IDE window as well as the selected project upon button press in order 
-	 * to correctly identify which project the JDT will be grabbing from. 
-	 * @return The IPath representation of the currently active project.
-	 */
-	private IPath getProjectName() {
-		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+	public void addToTree() throws JavaModelException, CoreException {
 		IProject project = null;
 		IPath path = null;
 		
-	    if(window != null) { 
+		IWorkspaceRoot fileRoot = ResourcesPlugin.getWorkspace().getRoot();
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		if(window != null) { 
 	    	IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
 	    	if(selection instanceof IStructuredSelection) {
 				Object element = ((IStructuredSelection) selection).getFirstElement();
@@ -133,84 +92,28 @@ public class SampleHandler extends AbstractHandler {
 					project = jProject.getProject();
 				}
 			}
-	    path = project.getFullPath();
-	    }
-	    return path;
-	}
-	
-	/**
-	 * Uses the function getProjectName and gets the directory for the project
-	 * @return The IProject representation of the Project
-	 */
-	private IProject getProject() {
-		IProject project = getRoot().getProject(getProjectName().toOSString());
-		// Add to tree
-		return project;
-	}
-	
-	/**
-	 * Uses the IJavaPRoject representation of getProject and splits the project
-	 * into packages.
-	 * @return IPackageFragment representation of the project's packages 
-	 * @throws CoreException
-	 */
-	private IPackageFragment[] getPackages() throws CoreException {
-		if(getProject().isNatureEnabled("org.eclipse.jdt.core.javanature")) {
-			IJavaProject javaProject = JavaCore.create(getProject());
-			IPackageFragment[] packages = javaProject.getPackageFragments();
-			// Add to tree
-			return packages;
 		}
-		return null;
-	}
-	/**
-	 * Gets all compilable units in the project from each of the packages
-	 * @return ICompilationUnit representation of the classes in the project
-	 * @throws CoreException
-	 */
-	private ICompilationUnit[] getClasses() throws CoreException {
-		for(IPackageFragment aPackage : getPackages()) {
-			if(aPackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
-				
-				return aPackage.getCompilationUnits();
-				/*
-				for(ICompilationUnit aClass : aPackage.getCompilationUnits()) {
-					aClass.getElementName();
-					// Add to tree
-				}*/
-			}
-		}	
 		
-		return null;		
-	}
-	
-	/**
-	 * Returns all the methods in the class
-	 * *NOTE* This is a little buggy rn so let me fix it later
-	 * @return The methods in the project
-	 * @throws CoreException
-	 */
-	private String getMethods() throws CoreException {
-		String s = new String();
-		IMethod[] methodArray = new IMethod[1];
-		for(IPackageFragment aPackage : getPackages()) {
-			if(aPackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
-				for(ICompilationUnit aClass : aPackage.getCompilationUnits()) {
-					IType[] allTypes = aClass.getAllTypes();
-					
-					for(IType Type : allTypes) {
-						IMethod[] methods = Type.getMethods();
-						for(IMethod method : methods) {
-							// Add to tree
-							s += method.getElementName() + "\t";
-							//methodArray[methodArray.length] = method;
-						}
+	    path = project.getFullPath();
+	    project = fileRoot.getProject(path.toOSString());
+	    
+	    if(project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
+			IJavaProject javaProject = JavaCore.create(project);
+			IPackageFragment[] packages = javaProject.getPackageFragments();
+			for(IPackageFragment aPackage : packages) {
+				if(aPackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+					for(ICompilationUnit aClass : aPackage.getCompilationUnits()) {
+						// aClass iterates through all of the ICompilation Units
+						generateMethodAST(aClass);
+						generateStatementAST(aClass);
+						generateAttributeAST(aClass);
+						generateCommentAST(aClass);						
 					}
 				}
 			}
-		}	
-		return s;
-	}	
+	    }
+		
+	}
 	
 	
 	/**
