b086e0105aee6b843f0190036dbe0d52225419de
AndrewCMoore <andrewmoore9815@gmail.com>
2020-12-08 19:50:07 -0500

    Added getters and setters in Class.java

--- src/ssmc/Class.java
+++ src/ssmc/Class.java
@@ -10,19 +10,63 @@ public class Class {
 	private boolean critical;
 	private CompilationUnit originFile;
 	private Method[] methods;
-	private Attribute[] attribute;
+	private Attribute[] attributes;
 	
 	public Class(String identifier, CompilationUnit originFile) {
 		this.Identifier = identifier;
 		this.originFile = originFile;
 		this.serialized = false;
 		this.critical = false;
+		
 	}
 	
-	 // Method that finds the starting line and ending line of the class
+	// Method that finds the starting line and ending line of the class
 	// Use AttributeVisitor, MethodVisitor, and StatemenetVisitor to find all values within those lines
-   // Return the Methods, Attributes, complexity
-  // Line of A/M/S is within Class then put into array (compare)
+    // Return the Methods, Attributes, complexity
+    // Line of A/M/S is within Class then put into array (compare)
+	
+	// Getters
+	
+	public String getIdentifier() {
+		return this.Identifier;
+	}
+	public String getModifier() {
+		return this.modifier;
+	}
+	public boolean isSerialized() {
+		return this.serialized;
+	}
+	public boolean isCritical() {
+		return this.critical;
+	}
+	public CompilationUnit getCompilationUnit() {
+		return this.originFile;
+	}
+	public Method[] getMethods() {
+		return this.methods;
+	}
+	public Attribute[] getAttributes() {
+		return this.attributes;
+	}
+	
+	// Setters
+	
+	private void setModifier(String modifier) {
+		this.modifier = modifier;
+	}
+	public void setSerialized(boolean b) {
+		this.serialized = b;
+	}
+	public void setCritical(boolean b) {
+		this.critical = b;
+	}
+	public void addMethod(Method method) {
+		methods[methods.length] = method;
+	}
+	public void addAttribute(Attribute attribute) {
+		attributes[attributes.length] = attribute;
+	}
+	
 	
 }
 
