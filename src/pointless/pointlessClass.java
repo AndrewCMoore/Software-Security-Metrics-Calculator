7ae5cf83ba2f993bd52a93b2ac6f27d914564dc5
storm <storm@192.168.1.6>
2020-12-28 10:54:17 -0500

    forgot to add a public attribute example, added now

--- src/pointless/pointlessClass.java
+++ src/pointless/pointlessClass.java
@@ -2,11 +2,13 @@ package pointless;
 
 public class pointlessClass implements pointlessInterface {
 	
+	public int globalInteger;
+	
+	public pointlessClass() {}
+	public pointlessClass(int i) {globalInteger=i;}
 	
 	@Override
 	public void pointlessMethod() {
 		System.out.println("I'm Pointless");		
 	}
-	
-
 }
