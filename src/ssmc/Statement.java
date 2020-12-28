fcf642f25084d5f53d44fa725fbb81221c7f6575
AndrewCMoore <andrewmoore9815@gmail.com>
2020-12-28 10:38:07 -0500

    Added Statement.java

---
+++ src/ssmc/Statement.java
@@ -0,0 +1,36 @@
+package ssmc;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
+public class Statement {
+	private ASTNode nodeType;
+	private CompilationUnit compilationUnit;
+	private int complexityValue; 
+	private int startLine;
+	private int endLine;
+	
+	public Statement(ASTNode node, CompilationUnit compilationUnit) {
+		this.nodeType = node;
+		this.compilationUnit = compilationUnit; 
+		this.complexityValue = 0;
+		this.startLine = this.compilationUnit.getLineNumber(node.getStartPosition());
+		this.endLine = this.compilationUnit.getColumnNumber(node.getStartPosition() + node.getLength());
+	}
+	
+	public ASTNode getNode() {
+		return this.nodeType;
+	}
+	public CompilationUnit getCompilationUnit() {
+		return this.compilationUnit;
+	}
+	public int getComplexity() {
+		return this.complexityValue;
+	}
+	public int getStartLine() {
+		return this.startLine;
+	}
+	public int getEndLine() {
+		return this.endLine;
+	}
+}
