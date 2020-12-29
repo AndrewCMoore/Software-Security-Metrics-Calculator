7af210b8804d3f1f28bb03ef34e86547c4d0629d
AndrewCMoore <andrewmoore9815@gmail.com>
2020-12-29 10:02:55 -0500

    Changed Java Language to JLS15

--- src/ssmc/CAMValues.java
+++ src/ssmc/CAMValues.java
@@ -86,7 +86,7 @@ public class CAMValues {
 	
 	
 	protected static CompilationUnit parse(ICompilationUnit unit) {
-		ASTParser parser = ASTParser.newParser(AST.JLS13);
+		ASTParser parser = ASTParser.newParser(AST.JLS15);
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
 		parser.setSource(unit);
 		parser.setResolveBindings(true);
