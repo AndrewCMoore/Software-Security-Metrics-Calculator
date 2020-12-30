f919c442a13b2c1c98e3d7b846aa2322ecfbdfbc
AndrewCMoore <andrewmoore9815@gmail.com>
2020-12-30 13:05:28 -0500

    Fixed implementation of testing in Attribute_Teststest.java

--- src/Parser_Testing/Attribute_TestsTest.java
+++ src/Parser_Testing/Attribute_TestsTest.java
@@ -1,21 +1,34 @@
 package Parser_Testing;
 
+import java.util.ArrayList;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import ssmc.CAMValues;
+import ssmc.Attribute;
 
 class Attribute_TestsTest {
-
+	private ArrayList<Attribute> attributes;
 	public Attribute_TestsTest() throws CoreException{
 		MainTest mt = new MainTest();
-		ICompilationUnit cu = mt.AccessTestClass().getCompilationUnit("Attribute_Test");
+		ICompilationUnit cu = mt.AccessTestClass().getCompilationUnit("Attribute_Test.java");
 		// Now we can run CAM values
+		attributes = CAMValues.generateAttributeAST(cu);
+		
 	}
 	
 	
 	@Test
-	void test() {
-		
+	void testName() {
+		assertEquals("AttributeA", attributes.get(0).getIdentifier());
+	}
+	
+	@Test
+	void testModifier() {
+		assertEquals("Public ", attributes.get(0).getModifier());
 	}
 
 	
