d963b4b774c96e5a223858dde83b7bd127d48337
AndrewCMoore <andrewmoore9815@gmail.com>
2020-12-31 16:07:17 -0500

    Removed unused imports, added private varaible nodes and method getNodes() to class DeclarationVisitor.java

--- src/ssmc/DeclarationVisitor.java
+++ src/ssmc/DeclarationVisitor.java
@@ -2,51 +2,52 @@ package ssmc;
 
 import java.util.ArrayList;
 
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
-import org.eclipse.jdt.core.dom.BodyDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
-import org.eclipse.jdt.core.dom.ModuleDeclaration;
-import org.eclipse.jdt.core.dom.ReturnStatement;
 import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
-import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
 
 public class DeclarationVisitor extends ASTVisitor {
 	
+	
 	private CompilationUnit cu;
 	private static ArrayList<Class> classes;
+	private ArrayList<ASTNode> nodes;
 	
 	public DeclarationVisitor(CompilationUnit cu) {
 		 super();
 		 this.cu = cu;
 		 classes = new ArrayList<Class>();
+		 nodes = new ArrayList<ASTNode>();
 	}
 	
 	
-	 public boolean visit(TypeDeclaration node){
-		 	int startLineNum = ((CompilationUnit) node.getRoot()).getLineNumber(node.getStartPosition());
-		 	int endLineNum = ((CompilationUnit) node.getRoot()).getLineNumber(node.getStartPosition()+node.getLength() - 1);
-	        SimpleName name = node.getName();
-	        int modifiers = node.getModifiers();
-	        String modify = CAMValues.getModifier(modifiers);
-	        String id = name.toString();
-	        Class c = new Class(id,cu);
-	        c.setStartLine(startLineNum);
-	        c.setEndLine(endLineNum);
-	        c.setModifier(modify);
-	        
-	        classes.add(c);
-	        return true;
-	    }
-	 
-	
-
+	public boolean visit(TypeDeclaration node){
+		
+		nodes.add(node);
+		int startLineNum = ((CompilationUnit) node.getRoot()).getLineNumber(node.getStartPosition());
+		int endLineNum = ((CompilationUnit) node.getRoot()).getLineNumber(node.getStartPosition()+node.getLength() - 1);
+		SimpleName name = node.getName();
+		int modifiers = node.getModifiers();
+		String modify = CAMValues.getModifier(modifiers);
+		String id = name.toString();
+		Class c = new Class(id,cu);
+		c.setStartLine(startLineNum);
+		c.setEndLine(endLineNum);
+		c.setModifier(modify);
+		
+		classes.add(c);
+		return true;
+    }
 
 	public ArrayList<Class> getClasses(){
-			return classes;
-		}
+		return classes;
+	}
+	 
+	public ArrayList<ASTNode> getNodes(){
+		return nodes;
+	}
 	
 
 }
