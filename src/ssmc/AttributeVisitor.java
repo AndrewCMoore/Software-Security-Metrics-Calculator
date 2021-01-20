74f776c61d9b900065435cc65a141ff540514990
AndrewCMoore <andrewmoore9815@gmail.com>
2021-01-20 16:38:12 -0500

    Added JavaDoc for AttributeVisitor.java, addressing issue #42

--- src/ssmc/AttributeVisitor.java
+++ src/ssmc/AttributeVisitor.java
@@ -20,6 +20,11 @@ public class AttributeVisitor extends ASTVisitor{
 	private Set<String> names;
 	private ArrayList<ASTNode> nodes;
 	
+	/**
+	 * Constructor, calls the superclass ASTVisitor initializes ArrayList objects of Attribute, 
+	 * String, and ASTNode objects in order to track progress through the CompilationUnit.
+	 * @param compilationUnit The CompilationUnit the Visitor class will visit. 
+	 */
 	public AttributeVisitor(CompilationUnit compilationUnit) {
 		super();
 		this.compliationUnit = compilationUnit;
@@ -28,14 +33,40 @@ public class AttributeVisitor extends ASTVisitor{
 		nodes = new ArrayList<ASTNode>();
 	}
 	
+	/**
+	 * Returns an ArratyList of Attribute objects. These are all the Attribute objects created
+	 * when visiting the ASTNodes of this CompilationUnit.
+	 * @return ArrayList of Attribute objects
+	 */
 	public ArrayList<Attribute> getArrayList(){
 		return this.attributes;
 	}
 	
+	/**
+	 * Returns an ArrayList of ASTNode objects. These are all the ASTNode objects
+	 * created when visiting the ASTNodes of this CompilationUnit.
+	 * @return ArrayList of ASTNode objects
+	 */
 	public ArrayList<ASTNode> getNodes(){
 		return this.nodes;
 	}
 	
+	/**
+	 * This method is an overwritten method from the super class ASTVisitor. 
+	 * 
+	 * This method visits all nodes within the CompilationUnit that are of the Enum declaration
+	 * EnumConstantDeclaration follows the format:
+     * 		[ Javadoc ] { ExtendedModifier } Identifier
+     *  		[ ( [ Expression { , Expression } ] ) ]
+     *    		[ AnonymousClassDeclaration ]
+     *    
+     * Upon visiting these types, creates a new Attribute object. Sets the modifiers, 
+     * and add this ASTNode and Attribute object to their ArrayLists. This also adds the SimpleName 
+     * object of the ASTNode to an ArrayList. This is used in visit(SimpleName) to access the Attribute
+     * object that this SimpleName/ASTNode represents. 
+     * 
+     * Returns true to search for a child ASTNode.
+	 */
 	public boolean visit(EnumConstantDeclaration node) {
 		
 		SimpleName name = node.getName();											// Get the String ID of the node (variable)
@@ -55,6 +86,23 @@ public class AttributeVisitor extends ASTVisitor{
 		return true;
 	}
 	
+	/**
+	 * This method is an overwritten method from the super class ASTVisitor. 
+	 * 
+	 * This method visits all the SimpleName ASTNodes. These are Attribute usage statements
+	 * within the CompilationUnit. 
+	 * SimpleName ASTNodes follow the format of: 
+	 * 		
+	 * 		Identifier
+	 *
+	 * This class determines if the SimpleName is within the 
+	 * is in the ArrayList of SimpleNames. This indicates the varaible has been defined, and
+	 * is now checking for usage. 
+	 * 
+	 * Adds usage to the Attribute object. 
+	 * 
+	 * Returns true to search for a child ASTNode.
+	 */
 	public boolean visit(SimpleName node) {
 		
 		if (this.names.contains(node.getIdentifier())) {
@@ -71,6 +119,22 @@ public class AttributeVisitor extends ASTVisitor{
 		return true;
 	}
 	
+	/**
+	 * This method is an overwritten method from the super class ASTVisitor. 
+	 * 
+	 * This method visits all the VaraibleDeclarationFragment ASTNodes within 
+	 * the CompilationUnit. 
+	 * VaraibleDeclarationFragment ASTNodes follow the format of: 
+	 * 
+	 * 		Identifier { Dimension } [ = Expression ]
+	 * 
+	 * Upon visiting these types, it creates a new Attribute object. Sets the modifiers, 
+     * and add this ASTNode and Attribute object to their ArrayLists. This also adds the SimpleName 
+     * object of the ASTNode to an ArrayList. This is used in visit(SimpleName) to access the Attribute
+     * object that this SimpleName/ASTNode represents. 
+	 * 
+	 * Returns true to search for a child ASTNode.
+	 */
 	public boolean visit(VariableDeclarationFragment node) {
 		
 		SimpleName name = node.getName();											// Get the String ID of the node (variable)
