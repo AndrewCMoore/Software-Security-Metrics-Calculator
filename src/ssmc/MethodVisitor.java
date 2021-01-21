a09ec6cf55a826e0e09f83925b1701b94af7adae
AndrewCMoore <andrewmoore9815@gmail.com>
2021-01-20 19:58:14 -0500

    Added JavaDoc to MethodVisitor.java to ammend issue #42

--- src/ssmc/MethodVisitor.java
+++ src/ssmc/MethodVisitor.java
@@ -14,6 +14,10 @@ public class MethodVisitor extends ASTVisitor{
 	private CompilationUnit cu;
 	private ArrayList<ASTNode> nodes;
 	
+	/**
+	 * Consutructor for class
+	 * @param cu CompilationUnit
+	 */
     public MethodVisitor(CompilationUnit cu){
         super();
         this.cu = cu;
@@ -21,14 +25,51 @@ public class MethodVisitor extends ASTVisitor{
         nodes = new ArrayList<ASTNode>();
     }
 
+    /**
+     * Returns the Methods this class visits
+     * @return ArrayList of Method objects
+     */
     public ArrayList<Method> getMethods() {
 		return methods;
 	}
         
+    /**
+     * Returns the ASTNodes this class visits
+     * @return ArrayList of Method objects
+     */
     public ArrayList<ASTNode> getNodes() {
     	return nodes;
     }
     
+    /**
+	 * This method is an overwritten method from the super class ASTVisitor. 
+	 * 
+	 * This method visits all the MethodDeclaration ASTNodes within the CompilationUnit. 
+	 * TypeDeclaration ASTNodes is a combination of constructors and methods which follow 
+	 * the format of: 
+	 * 		
+	 *	MethodDeclaration:
+     *		[ Javadoc ] { ExtendedModifier } [ < TypeParameter { , TypeParameter } > ] ( Type | void )
+     *   		Identifier (
+     *       		[ ReceiverParameter , ] [ FormalParameter { , FormalParameter } ]
+     *   		) { Dimension }
+     *   		[ throws Type { , Type } ]
+     *   		( Block | ; )
+ 	 *
+ 	 *	ConstructorDeclaration:
+     *		[ Javadoc ] { ExtendedModifier } [ < TypeParameter { , TypeParameter } > ]
+     *  		Identifier (
+     *       		[ ReceiverParameter , ] [ FormalParameter { , FormalParameter } ]
+     *   		) { Dimension }
+     *   		[ throws Type { , Type } ]
+     * 			( Block | ; )
+	 *
+	 * This method determines key information of the Method in the 
+	 * CompilationUnt. It then creates a new Method object and sets 
+	 * these values within the class.
+	 * 
+	 * Returns true to search for a child ASTNode.
+	 */
     public boolean visit(MethodDeclaration node){  	
     	nodes.add(node);
     	int startLineNum = ((CompilationUnit) node.getRoot()).getLineNumber(node.getStartPosition());
