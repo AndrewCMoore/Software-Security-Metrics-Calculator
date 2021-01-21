61c920395b25c5a7d604b50d83a1d4d2f2273f07
Isaacpruner <Isaac.Pruner@gmail.com>
2021-01-21 13:48:48 -0500

    Added some more tests for JDTree

--- src/Parser_Testing/TestJDTree.java
+++ src/Parser_Testing/TestJDTree.java
@@ -1,6 +1,7 @@
 package Parser_Testing;
 
 import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
@@ -12,6 +13,7 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.junit.Test;
 import tree.JDTree;
+import tree.NodeType;
 
 public class TestJDTree {
 	
@@ -34,7 +36,115 @@ public class TestJDTree {
 				e.printStackTrace();
 			}
 			if(tree!=null) {
-				assert(tree.getLeefs().length==10);
+				assertEquals(tree.getLeefs().length,10);
+			}else {
+				fail();
+			}
+		}
+		
+		@Test
+		public void testNumberofLeefs2() {
+			IProject project = null;
+			IWorkspaceRoot fileRoot = ResourcesPlugin.getWorkspace().getRoot();
+			IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+
+			if (window != null) {
+				project = fileRoot.getProject("Single");
+
+			}
+			JDTree tree = null;
+			IJavaProject javaProject = JavaCore.create(project);
+			try {
+				 tree = new JDTree(javaProject,null);
+			} catch (JavaModelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(tree!=null) {
+				assertEquals(tree.getLeefs().length,1);
+			}else {
+				fail();
+			}
+		}
+		
+		@Test
+		public void testNumberofLeefs0() {
+			IProject project = null;
+			IWorkspaceRoot fileRoot = ResourcesPlugin.getWorkspace().getRoot();
+			IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+
+			if (window != null) {
+				project = fileRoot.getProject("None");
+
+			}
+			JDTree tree = null;
+			IJavaProject javaProject = JavaCore.create(project);
+			try {
+				 tree = new JDTree(javaProject,null);
+			} catch (JavaModelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(tree!=null) {
+				assertEquals(0,tree.getLeefs().length);
+			}else {
+				fail();
+			}
+		}
+		
+		@Test
+		public void testLeefType() {
+			IProject project = null;
+			IWorkspaceRoot fileRoot = ResourcesPlugin.getWorkspace().getRoot();
+			IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+
+			if (window != null) {
+				project = fileRoot.getProject("Parser-Testing");
+				
+
+			}
+			JDTree tree = null;
+			IJavaProject javaProject = JavaCore.create(project);
+			try {
+				 tree = new JDTree(javaProject,null);
+			} catch (JavaModelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(tree!=null) {
+				JDTree [] leafs = tree.getLeefs();
+				for(JDTree leaf : leafs) {
+					assertEquals(NodeType.CLASS,leaf.getType());
+				}
+			}else {
+				fail();
+			}
+		}
+		
+		@Test
+		public void testLeefType2() {
+			IProject project = null;
+			IWorkspaceRoot fileRoot = ResourcesPlugin.getWorkspace().getRoot();
+			IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+
+			if (window != null) {
+				project = fileRoot.getProject("Parser-Testing");
+				
+
+			}
+			JDTree tree = null;
+			IJavaProject javaProject = JavaCore.create(project);
+			try {
+				 tree = new JDTree(javaProject,null);
+			} catch (JavaModelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(tree!=null) {
+				JDTree [] leafs = tree.getLeefs();
+				for(JDTree leaf : leafs) {
+					assertEquals(ssmc.Class.class,leaf.getNode().getClass());
+				}
 			}else {
 				fail();
 			}
