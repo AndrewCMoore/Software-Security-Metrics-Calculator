6cc40634781433d7a97b3684de9db35d8a4216cb
Paul Hewson <phewson97@gmail.com>
2021-02-06 01:15:39 -0500

    add functionality to quality attributes and shell to design principles

--- src/Calculator/DesignPrincipals.java
+++ src/Calculator/DesignPrincipals.java
@@ -1,9 +1,133 @@
 package Calculator;
 
+import java.util.HashMap;
+
 public class DesignPrincipals {
+	private HashMap<String, Float> abstraction = new HashMap<String, Float>();
+	private HashMap<String, Float> cohesion = new HashMap<String, Float>();
+	private HashMap<String, Float> complexity = new HashMap<String, Float>();
+	private HashMap<String, Float> composition = new HashMap<String, Float>();
+	private HashMap<String, Float> coupling = new HashMap<String, Float>();
+	private HashMap<String, Float> designSize = new HashMap<String, Float>();
+	private HashMap<String, Float> encapsulation = new HashMap<String, Float>();
+	private HashMap<String, Float> inheritance = new HashMap<String, Float>();
+	private HashMap<String, Float> messaging = new HashMap<String, Float>();
+	private HashMap<String, Float> polymorphism = new HashMap<String, Float>();
+	private HashMap<String, Float> hierarchies = new HashMap<String, Float>();
 
 	public DesignPrincipals(PrimaryMetrics pm) {
-		// TODO Auto-generated constructor stub
+		abstraction(pm);
+		cohesion(pm);
+		complexity(pm);
+		composition(pm);
+		coupling(pm);
+		designSize(pm);
+		encapsulation(pm);
+		inheritance(pm);
+		messaging(pm);
+		polymorphism(pm);
+		hierarchies(pm);
 	}
+
+	private void hierarchies(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void polymorphism(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void messaging(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void inheritance(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void encapsulation(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void designSize(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void coupling(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void composition(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void complexity(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void cohesion(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void abstraction(PrimaryMetrics pm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public HashMap<String, Float> getAbstraction() {
+		return abstraction;
+	}
+
+	public HashMap<String, Float> getCohesion() {
+		return cohesion;
+	}
+	
+	public HashMap<String, Float> getComplexity() {
+		return complexity;
+	}
+
+	public HashMap<String, Float> getComposition() {
+		return composition;
+	}
+
+	public HashMap<String, Float> getCoupling() {
+		return coupling;
+	}
+
+	public HashMap<String, Float> getDesignSize() {
+		return designSize;
+	}
+
+	public HashMap<String, Float> getEncapsulation() {
+		return encapsulation;
+	}
+
+	public HashMap<String, Float> getInheritance() {
+		return inheritance;
+	}
+
+	public HashMap<String, Float> getMessaging() {
+		return messaging;
+	}
+
+	public HashMap<String, Float> getPolymorphism() {
+		return polymorphism;
+	}
+
+	public HashMap<String, Float> getHierarchies() {
+		return hierarchies;
+	}
+	
+	
 
 }
--- src/Calculator/QualityAttributes.java
+++ src/Calculator/QualityAttributes.java
@@ -1,9 +1,83 @@
 package Calculator;
 
+import java.util.HashMap;
+
 public class QualityAttributes {
+	private HashMap<String, Double> reusability = new HashMap<String, Double>();
+	private HashMap<String, Double> flexibility = new HashMap<String, Double>();
+	private HashMap<String, Double> understandability = new HashMap<String, Double>();
+	private HashMap<String, Double> functionality = new HashMap<String, Double>();
+	private HashMap<String, Double> extendability = new HashMap<String, Double>();
+	private HashMap<String, Double> effectiveness = new HashMap<String, Double>();
 
 	public QualityAttributes(DesignPrincipals dp) {
-		// TODO Auto-generated constructor stub
+		reusability(dp);
+		flexibility(dp);
+		understandability(dp);
+		functionality(dp);
+		extendability(dp);
+		effectiveness(dp);
 	}
+
+	private void reusability(DesignPrincipals dp) {
+		for(String key : dp.getAbstraction().keySet()) {
+			reusability.put(key, (-0.25 * dp.getCoupling().get(key)) + (0.25 * dp.getCohesion().get(key)) + (0.5 * dp.getMessaging().get(key)) + (0.5 * dp.getDesignSize().get(key)));
+		}
+	}
+
+	private void extendability(DesignPrincipals dp) {
+		for(String key : dp.getAbstraction().keySet()) {
+			extendability.put(key, (0.5 * dp.getAbstraction().get(key)) - (0.5 * dp.getCoupling().get(key)) + (0.5 * dp.getInheritance().get(key)) + (0.5 * dp.getPolymorphism().get(key)));
+		}
+	}
+
+	private void functionality(DesignPrincipals dp) {
+		for(String key : dp.getAbstraction().keySet()) {
+			functionality.put(key, (0.12 * dp.getCohesion().get(key)) + (0.22 * dp.getPolymorphism().get(key)) + (0.22 * dp.getMessaging().get(key)) + (0.22 * dp.getDesignSize().get(key)) + (0.22 * dp.getHierarchies().get(key)));
+		}
+	}
+
+	private void understandability(DesignPrincipals dp) {
+		for(String key : dp.getAbstraction().keySet()) {
+			understandability.put(key, (-0.33 * dp.getAbstraction().get(key)) + (0.33 * dp.getEncapsulation().get(key)) - (0.33 * dp.getCoupling().get(key)) + (0.33 * dp.getCohesion().get(key)) - (0.33 * dp.getPolymorphism().get(key)) - (0.33 * dp.getComplexity().get(key)) - (0.33 * dp.getDesignSize().get(key)));
+		}
+	}
+
+	private void flexibility(DesignPrincipals dp) {
+		for(String key : dp.getAbstraction().keySet()) {
+			flexibility.put(key, (0.25 * dp.getEncapsulation().get(key)) - (0.25 * dp.getCoupling().get(key)) + (0.5 * dp.getComposition().get(key)) + (0.5 * dp.getPolymorphism().get(key)));
+		}
+	}
+
+	private void effectiveness(DesignPrincipals dp) {
+		for(String key : dp.getAbstraction().keySet()) {
+			effectiveness.put(key, (0.2 * dp.getAbstraction().get(key)) + (0.2 * dp.getEncapsulation().get(key)) + (0.2 * dp.getComposition().get(key)) + (0.2 * dp.getInheritance().get(key)) + (0.2 * dp.getPolymorphism().get(key)));
+		}
+	}
+
+	public HashMap<String, Double> getReusability() {
+		return reusability;
+	}
+
+	public HashMap<String, Double> getFlexibility() {
+		return flexibility;
+	}
+
+	public HashMap<String, Double> getUnderstandability() {
+		return understandability;
+	}
+
+	public HashMap<String, Double> getFunctionality() {
+		return functionality;
+	}
+
+	public HashMap<String, Double> getExtendability() {
+		return extendability;
+	}
+
+	public HashMap<String, Double> getEffectiveness() {
+		return effectiveness;
+	}
+	
 
 }
