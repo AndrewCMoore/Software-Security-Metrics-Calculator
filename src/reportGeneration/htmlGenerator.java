0772530168fb5d29aef92c8dd8053bf2704f86c2
Isaacpruner <Isaac.Pruner@gmail.com>
2021-02-17 15:35:36 -0500

    Added functions for most HTML Sections
    Still to do, string them together into something usefull

--- src/reportGeneration/htmlGenerator.java
+++ src/reportGeneration/htmlGenerator.java
@@ -6,6 +6,20 @@ public class htmlGenerator {
 		// TODO access list of metrics
 	}
 
+	public String makeHead() {
+		String head="<!DOCTYPE html>\r\n"
+				+ "<html>\r\n"
+				+ "<head>\r\n"
+				+ "<meta charset=\"utf-8\">\r\n"
+				+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
+				+ "<title>Prototype</title>\r\n"
+				+ "<link rel=\"stylesheet\" href=\"circle.css\">\r\n"
+				+ "<link rel=\"stylesheet\" href=\"style.css\">\r\n"
+				+ "</head>";
+		return head;
+	}
+	
+	
 	public String makeNavBar() {
 		String navBar = "<nav class=\"navbar\">"
 					+	"<button class=\"navButton\" onclick=\"expand()\">Open Menu</button>"
@@ -21,6 +35,8 @@ public class htmlGenerator {
 					+"</nav>";
 		return navBar;
 	}
+	
+	
 
 	public String makeCircle(String name, String number, String cssClass) {
 		String c = "";
@@ -51,6 +67,40 @@ public class htmlGenerator {
 		}
 		t += "</table>\n";
 		return t;
+	}
+	
+	public String makeJavaScriptSection(){
+		String script ="<script>\r\n"
+				+ "		function collapse(){\r\n"
+				+ "			button = event.target;\r\n"
+				+ "			console.log(button)\r\n"
+				+ "			console.log(button.getAttribute('id')+\"Section\");\r\n"
+				+ "			section = document.getElementById(button.getAttribute('id')+\"Section\");\r\n"
+				+ "			console.log(section);\r\n"
+				+ "			if(section.classList.contains('close')){\r\n"
+				+ "				section.classList.remove('close');\r\n"
+				+ "				section.classList.add('open');\r\n"
+				+ "			}else if(section.classList.contains('open')){\r\n"
+				+ "				section.classList.remove('open');\r\n"
+				+ "				section.classList.add('close');\r\n"
+				+ "			}\r\n"
+				+ "			\r\n"
+				+ "		}\r\n"
+				+ "		function expand(){\r\n"
+				+ "			section = document.getElementById(\"navBar\");\r\n"
+				+ "			console.log(section);\r\n"
+				+ "			if(section.classList.contains('collapse')){\r\n"
+				+ "				section.classList.remove('collapse');\r\n"
+				+ "				section.classList.add('expand');\r\n"
+				+ "			}else if(section.classList.contains('expand')){\r\n"
+				+ "				section.classList.remove('expand');\r\n"
+				+ "				section.classList.add('collapse');\r\n"
+				+ "			}\r\n"
+				+ "			\r\n"
+				+ "		}\r\n"
+				+ "	</script>";
+		
+		return script;
 	}
 
 }
